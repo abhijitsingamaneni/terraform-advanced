4b96be6c3fa279a8b4803093fff8e3f4bf7b83ae
Abhijit chowdary Singamaneni <abhijitcs1417@gmail.com>
2018-08-13 16:06:45 -0700

    Update main.tf

--- IAM_role/modules/policy/s3/fullaccess/main.tf
+++ IAM_role/modules/policy/s3/fullaccess/main.tf
@@ -1,3 +1,8 @@
+#Data sources allow data to be fetched or computed for use elsewhere in Terraform configuration. 
+#Use of data sources allows a Terraform configuration to build on information defined outside of Terraform, 
+#or defined by another separate Terraform configuration.
+# Thanks to terraform docs https://www.terraform.io/docs/configuration/data-sources.html
+# so we are using this data soure to call the data into aws_iam_policy resource
 data "aws_iam_policy_document" "s3_full_access" {
   statement {
     "effect"="Allow",
@@ -10,6 +15,7 @@ data "aws_iam_policy_document" "s3_full_access" {
   }
 }
 
+# This resource is used to create a new policy using data
 resource "aws_iam_policy" "s3_full_access" {
   name   = "s3_full_access"
   path   = "/"
