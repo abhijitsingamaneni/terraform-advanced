3e5c88f56ece31f1afe3767c13ef310bd2bfb5be
Abhijit chowdary Singamaneni <abhijitcs1417@gmail.com>
2018-09-04 17:14:15 -0700

    Update main.tf

--- Rolling_deploy/modules/autoscaling/main.tf
+++ Rolling_deploy/modules/autoscaling/main.tf
@@ -4,10 +4,10 @@
 
 resource "aws_launch_configuration" "ec2-launch-configuration" {
     name_prefix                 = "${terraform.workspace}-"
-    image_id                    = "${var.ami_ID}"
-    instance_type               = "${var.type}"
+    image_id                    = "${var.ami_id}"
+    instance_type               = "${var.instance_type}"
     security_groups             = ["${split(",", var.security_group)}"]
-    key_name                    = "${var.key}"
+    key_name                    = "${var.key_pair}"
     iam_instance_profile        = "${var.instance_profile}"
 
     root_block_device {
@@ -28,19 +28,16 @@ resource "aws_launch_configuration" "ec2-launch-configuration" {
 
 resource "aws_autoscaling_group" "ec2-autoscaling-group" {
     name_prefix                 = "${terraform.workspace}-${var.ami_ID}"
-    max_size                    = "${var.max_instance_size}"
-    min_size                    = "${var.min_instance_size}"
+    max_size                    = "${var.max_instances}"
+    min_size                    = "${var.min_instances}"
     desired_capacity            = "${var.desired_capacity}"
     min_elb_capacity            = "${var.elb_capacity}"
     health_check_grace_period   = "${var.grace_period}"
     health_check_type           = "${var.health_check}"
-    load_balancers              = ["${split(",", var.lb)}"]
+    load_balancers              = ["${split(",", var.loadbalancer)}"]
     vpc_zone_identifier         = ["${split(",", var.subnets)}"]
     launch_configuration        =
     "${aws_launch_configuration.ec2-launch-configuration.id}"
-
-    tags = ["${concat(var.default_tags)}"]
-
     lifecycle {
       create_before_destroy = true
     }
